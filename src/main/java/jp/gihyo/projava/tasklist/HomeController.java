f17d6446b24a22298ada67517cd30e29f7a32062
kuonhasegawa <kuon.hasegawa@gmail.com>
2023-08-25 14:24:12 +0900

    個人用タスク管理アプリケーションあいうえおあいうえお2

--- src/main/java/jp/gihyo/projava/tasklist/HomeController.java
+++ src/main/java/jp/gihyo/projava/tasklist/HomeController.java
@@ -2,11 +2,11 @@ package jp.gihyo.projava.tasklist;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -15,42 +15,67 @@ import java.util.UUID;
 
 @Controller
 public class HomeController {
+
     private final TaskListDao dao;
+
     @Autowired
     HomeController(TaskListDao dao){
         this.dao = dao;
     }
-    record TaskItem(String id,String task, String deadline,boolean done){}
-    private List<TaskItem> taskItems = new ArrayList<>();
+    @RequestMapping(value="/hello")
+    String hello(Model model){
+        model.addAttribute("time", LocalDateTime.now());
+        return "hello";
+    }
 
-    @RequestMapping("/list")
+    record TaskItem(String id, String task,String deadline, String memo, boolean done){}
+//    private List<TaskItem> taskItems = new ArrayList<>();
+
+    @GetMapping("/list")
     String listItems(Model model){
-        List<TaskItem> taskItems =dao.findAll();
-        model.addAttribute("taskList",taskItems);
+        List<TaskItem> taskItems = this.dao.findAll();
+        model.addAttribute("taskList", taskItems);
         return "home";
     }
 
     @GetMapping("/add")
-    String addItem(@RequestParam("task")String task,
+    String addItem(@RequestParam("task") String task,
                    @RequestParam("deadline") String deadline){
-        String id = UUID.randomUUID().toString().substring(0,8);
-        TaskItem item = new TaskItem(id, task, deadline, false);
-        dao.add(item);
+        String id = UUID.randomUUID().toString().substring(0, 8);
+        TaskItem item = new TaskItem(id, task, deadline, "",false);
+        this.dao.add(item);
+        return "redirect:/list";
+    }
 
-        return "redirect:/list";
-    }
     @GetMapping("/delete")
-    String deleteItem(@RequestParam("id") String id){
-        dao.delete(id);
+    String delete(@RequestParam("id") String id){
+        this.dao.delete(id);
         return "redirect:/list";
     }
+
     @GetMapping("/update")
-    String updateItem(@RequestParam("id") String id,
-                      @RequestParam("task") String task,
-                      @RequestParam("deadline") String deadline,
-                      @RequestParam("done") boolean done){
-        TaskItem taskItem = new TaskItem(id,task,deadline,done);
-        dao.update(taskItem);
+    String update(@RequestParam("id") String id,
+                  @RequestParam("task") String task,
+                  @RequestParam("deadline") String deadline,
+                  @RequestParam("memo") String memo,
+                  @RequestParam("done") boolean done){
+        TaskItem item = new TaskItem(id, task, deadline, memo, done);
+        int i = this.dao.update(item);
         return "redirect:/list";
     }
+
+    @GetMapping("/search_month")
+    String searchMonth(Model model, @RequestParam("month") String month,
+                       @RequestParam("complete") String complete){
+        List<TaskItem> taskItems = this.dao.searchMonth(month,complete);
+        model.addAttribute("taskList", taskItems);
+
+        return "home";
+    }
+//    @GetMapping("/search_complete")
+//    String complete(Model model, @RequestParam("complete") String complete){
+//        List<TaskItem> taskItems = this.dao.search_complete(complete);
+//        model.addAttribute("taskList", taskItems);
+//        return "home";
+//    }
 }
--- src/main/java/jp/gihyo/projava/tasklist/TaskListDao.java
+++ src/main/java/jp/gihyo/projava/tasklist/TaskListDao.java
@@ -1,21 +1,18 @@
 package jp.gihyo.projava.tasklist;
 
-import jp.gihyo.projava.tasklist.HomeController.TaskItem;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Service;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.util.List;
 import java.util.Map;
 
-
 @Service
 public class TaskListDao {
-
+    private final static String TABLE_NAME = "tasklist";
     private final JdbcTemplate jdbcTemplate;
 
     @Autowired
@@ -23,33 +20,40 @@ public class TaskListDao {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    public void add(TaskItem taskItem){
-        SqlParameterSource param = new BeanPropertySqlParameterSource(taskItem);
-        SimpleJdbcInsert insert =
-                new SimpleJdbcInsert(jdbcTemplate)
-                        .withTableName("tasklist");
-        insert.execute(param);
+    /**
+     * レコードを追加し、追加した件数を返す
+     * @param item HomeRestController.TaskItem
+     * @return 追加件数
+     */
+    public int add(HomeController.TaskItem item){
+        SqlParameterSource param = new BeanPropertySqlParameterSource(item);
+        SimpleJdbcInsert insert = new SimpleJdbcInsert(this.jdbcTemplate)
+                .withTableName(TABLE_NAME);
+        return insert.execute(param);
     }
 
-    public List<TaskItem> findAll(){
-        String query = "SELECT * FROM tasklist";
-        List<Map<String,Object>> result = jdbcTemplate.queryForList(query);
-        List<TaskItem> taskItems = result.stream()
-                .map((Map<String,Object> row) -> new TaskItem(
+    public <LIst> List<HomeController.TaskItem> findAll(){
+        String query = "SELECT * FROM " + TABLE_NAME;
+        List<Map<String, Object>> result = this.jdbcTemplate.queryForList(query);
+        List<HomeController.TaskItem> list = result.stream().map(
+                (Map<String, Object> row) -> new HomeController.TaskItem(
                         row.get("id").toString(),
                         row.get("task").toString(),
                         row.get("deadline").toString(),
                         row.get("memo").toString(),
-                        (Boolean)row.get("done")))
-                .toList();
-        return taskItems;
+                        (Boolean)row.get("done")
+
+                )).toList();
+        return list;
     }
+
     public int delete(String id){
-        int number = jdbcTemplate.update("DELETE FROM tasklist WHERE id = ?", id);
-        return number;
+        int num = jdbcTemplate.update("DELETE FROM " + TABLE_NAME + " WHERE id = ?", id);
+        return num;
     }
-    public int update(TaskItem taskItem){
-        int number = jdbcTemplate.update("UPDATE tasklist SET task = ?, deadline = ?,memo = ?,done = ? WHERE id = ?",
+
+    public int update(HomeController.TaskItem taskItem){
+        int number = jdbcTemplate.update("update tasklist set task=?, deadline=?, memo=?, done=? where id = ?",
                 taskItem.task(),
                 taskItem.deadline(),
                 taskItem.memo(),
@@ -57,4 +61,46 @@ public class TaskListDao {
                 taskItem.id());
         return number;
     }
+
+    public <LIst> List<HomeController.TaskItem> searchMonth(String month, String complete){
+        String query;
+
+        if (complete.equals("0")){
+            query = "SELECT * FROM " + TABLE_NAME + " WHERE deadline like '" + month + "%'";
+        }else{
+            query = "SELECT * FROM " + TABLE_NAME  + " WHERE deadline like '" + month + "%'"+ "and done=0";
+        }
+        List<Map<String, Object>> result = this.jdbcTemplate.queryForList(query);
+        List<HomeController.TaskItem> list = result.stream().map(
+                (Map<String, Object> row) -> new HomeController.TaskItem(
+                        row.get("id").toString(),
+                        row.get("task").toString(),
+                        row.get("deadline").toString(),
+                        row.get("memo").toString(),
+                        (Boolean)row.get("done")
+
+                )).toList();
+        return list;
+    }
+//    public List<HomeController.TaskItem> search_complete(String complete) {
+//        String query;
+//
+//        if (complete.equals("0")){
+//            query = "SELECT * FROM " + TABLE_NAME;
+//        }else{
+//            query = "SELECT * FROM " + TABLE_NAME + " WHERE done=0 ";
+//        }
+//
+//            List<Map<String, Object>> result = this.jdbcTemplate.queryForList(query);
+//            List<HomeController.TaskItem> list = result.stream().map(
+//                    (Map<String, Object> row) -> new HomeController.TaskItem(
+//                            row.get("id").toString(),
+//                            row.get("task").toString(),
+//                            row.get("deadline").toString(),
+//                            row.get("memo").toString(),
+//                            (Boolean)row.get("done")
+//
+//                    )).toList();
+//            return list;
+//        }
 }
